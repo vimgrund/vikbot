1f9da898f4a5372ddf73d63389aa0a2a41deb3c1
Viktor Imgrund <viktor.imgrund@outlook.de>
2023-01-08 22:52:07 +0100

    es lebt

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ import json
 import requests
 import os
 from heartbeat import start_heartbeat
-from utils import download_pic
+from utils import download_pic, ftp_upload
 
 
 def main():
-    masterAccounts = os.environ['MASTER_ACCOUNTS'].split(',')
+    masterAccounts = os.environ['VB_MASTER_ACCOUNTS'].split(',')
     intents = discord.Intents.default()
     intents.message_content = True
     bot = discord.Client(intents=intents)
@@ -26,7 +26,7 @@ def main():
 
         if author not in masterAccounts:
             await msg.channel.send(f"Hallo {author_name}, scheint, als wolltest du mich zum Arbeiten animieren... ich höre aber nur auf meine Meister q-:")
-            return 
+            return
 
         if msg.content == "!ping":
             await msg.channel.send("pong")
@@ -36,23 +36,23 @@ def main():
             for pic in msg.attachments:
                 url = pic.url
                 filename = download_pic(url)
-                await msg.channel.send(f"Hallo {author_name}, file {filename} gespeichert")
+                link = ftp_upload(filename, prefix="/vikbot/")
+                os.remove(filename)
+                await msg.channel.send(f"Hallo {author_name}, file {filename.split('/')[-1]} gespeichert\n{link}")
             return
 
         if msg.content == "!help":
-            await msg.channel.send(f"Hallo {author_name}, noch kann ich nicht viel...")
-            await msg.channel.send("mit \"!sticker\" kannst du mir ein Bild schicken, dass ich dann erstmal speicher und im nächsten Entwicklungsschritt automatisch auf die Homepage hochlade")
-            await msg.channel.send("mit \"!help\" bekommst du genau wieder die gleiche Antwort")
-            await msg.channel.send("\"!ping\" beantworte ich mit \"pong\"")
+            await msg.channel.send(f"Hallo {author_name}, noch kann ich nicht viel..."
+                                   + "\nmit \"!sticker\" kannst du mir eine Datei schicken, dass ich dann erstmal speicher und im nächsten Entwicklungsschritt automatisch auf die Homepage hochlade"
+                                   + "\nmit \"!help\" bekommst du genau wieder die gleiche Antwort"
+                                   + "\n\"!ping\" beantworte ich mit \"pong\"")
             return
 
         await msg.channel.send(f"Hallo {author_name}, wie kann ich dienen? \nTippe \"!help\" für eine Beschreibung meiner Fähigkeiten")
 
-
-    start_heartbeat()
-    DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+    # start_heartbeat()
+    DISCORD_TOKEN = os.environ['VB_DISCORD_TOKEN']
     bot.run(DISCORD_TOKEN)
-
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,34 @@
+import pysftp
 import requests
+import os
+
 
 def download_pic(url):
-    filename = url.split('/')[-1]
+    filename = "../tmp/" + url.split('/')[-1]
     res = requests.get(url)
     with open(filename, "wb") as f:
         f.write(res.content)
     return filename
+
+
+def ftp_upload(file, prefix=""):
+    # credentials of targeted sftp server
+    host = os.environ['VB_FTP_HOST']
+    username = os.environ['VB_FTP_USER']
+    password = os.environ['VB_FTP_PW']
+    url = os.environ['VB_URL']
+    result = "¯\_ (ツ)_/¯"
+    try:
+        with pysftp.Connection(host=host,
+                               username=username, password=password) as conn:
+            print("connection established successfully")
+            # file path of local file and targeted location
+            filename = file.split('/')[-1]
+            target_location = f'/httpdocs/{prefix}{filename}'
+            # call conn.put() method to upload file to server
+            conn.put(file, target_location)
+            conn.close()
+            result = f"{url}{prefix}{filename}"
+    except:
+        result = 'failed to establish connection to targeted server'
+    return result
